bbf0cc84564bb903176e57308bfab7b42d453363
sanggam12 <161117617+sanggam12@users.noreply.github.com>
2024-04-27 16:33:35 +0700

    Create data_mahasiswa.php
    Biodata

---
+++ data_mahasiswa.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "NIM = 22110038";
+echo "Nama Lengkap = Sanggam Yohendra";
+echo "Tempat Tanggal Lahir = Pekanbaru";
+echo "Tanggal Lahir = 03 Oktober 2000";
+echo "Alamat = Jl Pandau Jaya Pasir Putih";
+
+?>
