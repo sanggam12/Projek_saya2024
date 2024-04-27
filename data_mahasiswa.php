9461d5924419e9e80fa8d0035bf36a8b7965bc65
sanggam12 <161117617+sanggam12@users.noreply.github.com>
2024-04-27 16:40:35 +0700

    Update data_mahasiswa.php

--- data_mahasiswa.php
+++ data_mahasiswa.php
@@ -1,9 +1,13 @@
 <?php
 
+echo "<h1>Data Mahasiswa</h1>";
 echo "NIM = 22110038";
 echo "Nama Lengkap = Sanggam Yohendra";
 echo "Tempat Tanggal Lahir = Pekanbaru";
 echo "Tanggal Lahir = 03 Oktober 2000";
 echo "Alamat = Jl Pandau Jaya Pasir Putih";
+echo "Kecamatan = Siak Hulu";
+echo "Kabupaten = Kampar";
+echo "Kelas = 22MI B";
 
 ?>
